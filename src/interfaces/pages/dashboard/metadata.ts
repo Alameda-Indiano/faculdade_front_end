16980e78e6c8bc25b2ce52bfbe135f07a6c15986
Gabriel Russo <gabriel_augusto_russo@hotmail.com>
2025-05-03 16:41:20 -0300

    cria CRUDS de assinatura, frequencia e histórico de pagamento

--- src/interfaces/pages/dashboard/metadata.ts
+++ src/interfaces/pages/dashboard/metadata.ts
@@ -1,6 +1,6 @@
 import { IMetadata } from '../../../types/IMetadata';
 
-export const metadataUser: IMetadata = {
+export const metadataDashboard: IMetadata = {
 	title: 'Dashboard',
 	description: 'Configuração de Dashboard do Sistema',
 	robots: 'noindex, nofollow',
